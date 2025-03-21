abd505bd509dc08962b0fd7f463efa8fb871331b
1018-D <124874486+DNG-021018@users.noreply.github.com>
2025-03-21 21:51:38 +0700

    UPDATE Models

--- Assets/Script/Models/Enemy.cs
+++ Assets/Script/Models/Enemy.cs
@@ -1,15 +1,28 @@
 namespace dang
 {
-    public class Enemy : Entities
+    public class Enemy
     {
+        public string entityType;
+        public int maxHealth;
+        public float runSpeed;
+        public float attackDamage;
+        public float attackRange;
+        public float attackSpeed;
+
+        public Enemy(string entityType, int maxHealth, float runSpeed, float attackDamage, float attackRange, float attackSpeed)
+        {
+            this.entityType = entityType;
+            this.maxHealth = maxHealth;
+            this.runSpeed = runSpeed;
+            this.attackDamage = attackDamage;
+            this.attackRange = attackRange;
+            this.attackSpeed = attackSpeed;
+        }
+
         public Enemy()
         {
-            entityType = "Enemy";
-            maxHealth = 100;
-            runSpeed = 5.0f;
-            attackDamage = 10.0f;
-            attackRange = 1.0f;
-            attackSpeed = 1.0f;
         }
+
+
     }
 }
